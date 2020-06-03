5370c6ac94ad7957801ae3fd4f8bd15c4898273c
Nachu13 <nachiketa.p.iyer@outlook.com>
2020-06-03 15:50:39 +0530

    created Constraints

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint; 
 
-var engine, world;
+var engine, world, chain;
 var box1, pig1;
 var backgroundImg,platform;
 
@@ -14,7 +15,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -29,13 +30,16 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
+    log2 = new Log(230, 180, 80, PI/2);
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,log2.body);
 
+
+    
 }
 
 function draw(){
@@ -54,11 +58,12 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+    log2.display();
     box5.display();
     log4.display();
     log5.display();
-
+    chain.display();
     bird.display();
+    
     platform.display();
 }
